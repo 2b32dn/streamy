942502c56742cbdd8a60758552946239c1ef2a81
Dickson <Dicksont_lee@hotmail.com>
2019-03-19 18:34:18 -0400

    third

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 
 import App from './components/App'
+import reducers from './reducers'
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.querySelector('#root'))
+const store = createStore(reducers)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>, 
+  document.querySelector('#root')
+)
