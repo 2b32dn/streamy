46f7d4df201b6a75e583f54d6e2214a3894479fd
Dickson <Dicksont_lee@hotmail.com>
2019-03-20 17:40:17 -0400

    api

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,13 @@
 import {combineReducers} from 'redux'
 import authReducers from './authReducers'
+import streamReducer from './streamReducer'
+
+// Redux-form
+// formReducer is the given name we export reducer as
+import { reducer as formReducer } from 'redux-form'
 
 export default combineReducers({
-  auth: authReducers
+  auth: authReducers,
+  form: formReducer,
+  streams: streamReducer
 })
